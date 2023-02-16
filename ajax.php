7997f24391ab818940419c136ab130a8b9c91cc5
Sebastixi <3btixilemasebastian@gmail.com>
2023-02-16 16:11:46 -0500

    changes in the design of game.php and inclusion of a part of the functions

--- ajax.php
+++ ajax.php
@@ -1,35 +1,38 @@
 <?php
-session_start();
-//Enter a new Word on the session variable
-if (isset($_POST['option']) && $_POST['option']=="enterWord"){
-    $message = "word not entered";
-    if (!isset($_SESSION['Words'])) {
-        $_SESSION['Words'][0] = $_POST['word'];
-        $message = "Word entered!";
-    } else {
-
-        $countWords = count($_SESSION['Words']);
-        if($countWords>=10){
-            $message = "You can only put 10 words";
-        }else{
-            $_SESSION['Words'][$countWords] = $_POST['word'];
+    session_start();
+    //Enter a new Word on the session variable
+    if (isset($_POST['option']) && $_POST['option']=="enterWord"){
+        $message = "word not entered";
+        if (!isset($_SESSION['Words'])) {
+            $_SESSION['Words'][0] = $_POST['word'];
             $message = "Word entered!";
-        }
-        
-    }
-    echo($message);
-}
-if (isset($_POST['option']) && $_POST['option']=="eraseWords"){
-    $message = "Words erased";
-    if (!isset($_SESSION['Words'])) {
+        } else {
 
-    } else {
-        $_SESSION['Words'] = '';
-        session_unset();
-        session_destroy();
-        
+            $countWords = count($_SESSION['Words']);
+            if($countWords>=10){
+                $message = "You can only put 10 words";
+            }else{
+                $_SESSION['Words'][$countWords] = $_POST['word'];
+                $message = "Word entered!";
+            }
+            
+        }
+        echo($message);
     }
-    echo($message);
-}
+    //Erase all the words for the list
+    if (isset($_POST['option']) && $_POST['option']=="eraseWords"){
+        $message = "Words erased";
+        if (!isset($_SESSION['Words'])) {
+
+        } else {
+            $_SESSION['Words'] = '';
+            session_unset();
+            session_destroy();
+            
+        }
+        echo($message);
+    }
+    //Check a letter in the word
+
 
 ?>
--- game.php
+++ game.php
@@ -19,7 +19,84 @@ session_start();?>
         <script src="jquery.js"></script>
         <script>
             $(document).ready(function(){ 
-                
+                $("#btnA").click(function(){
+                     $(this).hide();
+                });
+                $("#btnB").click(function(){
+                     $(this).hide();
+                });
+                $("#btnC").click(function(){
+                     $(this).hide();
+                });
+                $("#btnD").click(function(){
+                     $(this).hide();
+                });
+                $("#btnE").click(function(){
+                     $(this).hide();
+                });
+                $("#btnF").click(function(){
+                     $(this).hide();
+                });
+                $("#btnG").click(function(){
+                     $(this).hide();
+                });
+                $("#btnH").click(function(){
+                     $(this).hide();
+                });
+                $("#btnI").click(function(){
+                     $(this).hide();
+                });
+                $("#btnJ").click(function(){
+                     $(this).hide();
+                });
+                $("#btnK").click(function(){
+                     $(this).hide();
+                });
+                $("#btnL").click(function(){
+                     $(this).hide();
+                });
+                $("#btnM").click(function(){
+                     $(this).hide();
+                });
+                $("#btnN").click(function(){
+                     $(this).hide();
+                });
+                $("#btnO").click(function(){
+                     $(this).hide();
+                });
+                $("#btnP").click(function(){
+                     $(this).hide();
+                });
+                $("#btnQ").click(function(){
+                     $(this).hide();
+                });
+                $("#btnR").click(function(){
+                     $(this).hide();
+                });
+                $("#btnS").click(function(){
+                     $(this).hide();
+                });
+                $("#btnT").click(function(){
+                     $(this).hide();
+                });
+                $("#btnU").click(function(){
+                     $(this).hide();
+                });
+                $("#btnV").click(function(){
+                     $(this).hide();
+                });
+                $("#btnW").click(function(){
+                     $(this).hide();
+                });
+                $("#btnX").click(function(){
+                     $(this).hide();
+                });
+                $("#btnY").click(function(){
+                     $(this).hide();
+                });
+                $("#btnZ").click(function(){
+                     $(this).hide();
+                });
             });
         </script>
     </head>
@@ -34,8 +111,8 @@ session_start();?>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link" href="#page-top">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#signup">Start The game</a></li>
+                        <li class="nav-item"><a class="nav-link">Next Word</a></li>
+                        <li class="nav-item"><a class="nav-link">End The game</a></li>
                     </ul>
                 </div>
             </div>
@@ -45,13 +122,60 @@ session_start();?>
             <div class="container px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-lg-8">
-                        <h2 class="text-white mb-4">Put a word and confirm it</h2>
-                        <div class="form-group">
-                            <input type="text" class="form-control" id="inputWords" aria-describedby="textHelp" placeholder="Enter a Word">
-                            <small id="textHelp" class="form-text text-muted">you need to press enter to confirm the word.</small>
-                        </div>
+                        <h2 class="text-white mb-4">Hangman Game</h2>
+                        <img src="assets/hangman0.png" class="img-fluid" alt="Responsive image" width="250" height="300" id="hangmanImg">
                         <br>
-                        <button type="button" class="btn btn-success" id="btnSuccess">Enter</button>
+                        <table class="table table-bordered table-dark">
+                            <thead>
+                                <tr>
+                                    <td>A</td>
+                                    <td>R</td>
+                                    <td>B</td>
+                                    <td>O</td>
+                                    <td>L</td>
+                                </tr>
+                            </thead>
+                        </table>
+                        <br>
+                        <table class="table table-striped table-dark">
+                            <tbody>
+                                <tr>
+                                    <td><button type="button" class="btn btn-secondary btn-sm" id="btnA">A</button></td>
+                                    <td><button type="button" class="btn btn-secondary btn-sm" id="btnB">B</button></td>
+                                    <td><button type="button" class="btn btn-secondary btn-sm" id="btnC">C</button></td>
+                                    <td><button type="button" class="btn btn-secondary btn-sm" id="btnD">D</button></td>
+                                    <td><button type="button" class="btn btn-secondary btn-sm" id="btnE">E</button></td>
+                                    <td><button type="button" class="btn btn-secondary btn-sm" id="btnF">F</button></td>
+                                    <td><button type="button" class="btn btn-secondary btn-sm" id="btnG">G</button></td>
+                                    <td><button type="button" class="btn btn-secondary btn-sm" id="btnH">H</button></td>
+                                    <td><button type="button" class="btn btn-secondary btn-sm" id="btnI">I</button></td>
+                                </tr>
+                                <tr>
+                                    <td><button type="button" class="btn btn-secondary btn-sm" id="btnJ">J</button></td>
+                                    <td><button type="button" class="btn btn-secondary btn-sm" id="btnK">K</button></td>
+                                    <td><button type="button" class="btn btn-secondary btn-sm" id="btnL">L</button></td>
+                                    <td><button type="button" class="btn btn-secondary btn-sm" id="btnM">M</button></td>
+                                    <td><button type="button" class="btn btn-secondary btn-sm" id="btnN">N</button></td>
+                                    <td><button type="button" class="btn btn-secondary btn-sm" id="btnO">O</button></td>
+                                    <td><button type="button" class="btn btn-secondary btn-sm" id="btnP">P</button></td>
+                                    <td><button type="button" class="btn btn-secondary btn-sm" id="btnQ">Q</button></td>
+                                    <td><button type="button" class="btn btn-secondary btn-sm" id="btnR">R</button></td>
+                                </tr>
+                                <tr>
+                                    <td><button type="button" class="btn btn-secondary btn-sm" id="btnS">S</button></td>
+                                    <td><button type="button" class="btn btn-secondary btn-sm" id="btnT">T</button></td>
+                                    <td><button type="button" class="btn btn-secondary btn-sm" id="btnU">U</button></td>
+                                    <td><button type="button" class="btn btn-secondary btn-sm" id="btnV">V</button></td>
+                                    <td><button type="button" class="btn btn-secondary btn-sm" id="btnW">W</button></td>
+                                    <td><button type="button" class="btn btn-secondary btn-sm" id="btnX">X</button></td>
+                                    <td><button type="button" class="btn btn-secondary btn-sm" id="btnY">Y</button></td>
+                                    <td><button type="button" class="btn btn-secondary btn-sm" id="btnZ">Z</button></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        
+                        <br>
+                        <button type="button" class="btn btn-success" id="btnContinue">Continue to the Next Word</button>
                         
                     </div>
                 </div>
